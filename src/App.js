76b665e082eccf5e96e810924f2dcad7c40e10a8
Prathmesh Sadake <prathmeshsadake@gmail.com>
2020-10-26 16:57:23 +0530

    Switch and Route import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Switch , Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component.jsx';
@@ -7,7 +8,11 @@ import HomePage from './pages/homepage/homepage.component.jsx';
 function App() {
   return (
     <div>
-      <HomePage/>
+      {/* Switch renders on one route witch matches. Useful when we  */}
+     <Switch>
+        {/* exact={ false } to keep exact as false default value is true */}
+      <Route exact path='/' component={HomePage}/>
+     </Switch>
     </div>
   );
 }
