5021d2cb151c219033eec99d5cc74ce8caa1a0b6
Prathmesh Sadake <prathmeshsadake@gmail.com>
2020-10-28 01:41:53 +0530

    imported shop component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch , Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component.jsx';
+import ShopPage from './pages/shop/shop.component';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
      <Switch>
         {/* exact={ false } to keep exact as false default value is true */}
       <Route exact path='/' component={HomePage}/>
+      <Route path='/shop' component={ShopPage}/>
      </Switch>
     </div>
   );
