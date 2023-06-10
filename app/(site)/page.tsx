698336ebef38455d89327533182279e72337215c
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-06-10 21:19:03 +0530

    Created layout component

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -4,7 +4,11 @@ import React from "react";
 const Home = () => {
   return (
     <div className='bg-neutral-900 rounded-lg overflow-hidden overflow-y-auto h-full w-full'>
-      <Header>Header</Header>
+      <Header>
+        <div className='mb-2'>
+          <h1 className='text-white font-semibold text-3xl'>Welcome back</h1>
+        </div>
+      </Header>
     </div>
   );
 };
