e8d64f78ed76c3baed392ffc7e76a99ee4dabab3
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-06-11 00:21:46 +0530

    Created ListItem component and completed App Layout

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,5 +1,5 @@
 import Header from "@/components/Header";
-import React from "react";
+import ListItem from "@/components/ListItem";
 
 const Home = () => {
   return (
@@ -8,7 +8,16 @@ const Home = () => {
         <div className='mb-2'>
           <h1 className='text-white font-semibold text-3xl'>Welcome back</h1>
         </div>
+        <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4'>
+          <ListItem image='/images/liked.png' name='Liked Songs' href='liked' />
+        </div>
       </Header>
+      <div className='mt-2 mb-7 px-6'>
+        <div className='flex justify-between items-center'>
+          <h1 className='font-semibold text-white text-2xl'>Newest Songs</h1>
+        </div>
+        <div>List on songs</div>
+      </div>
     </div>
   );
 };
