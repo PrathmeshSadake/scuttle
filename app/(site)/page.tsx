2b7a35023f36ba30f9579ac255bd8aaa1c33c9d9
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-06-10 21:09:56 +0530

    Created sidebar and header component

---
+++ app/(site)/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/Header";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className='bg-neutral-900 rounded-lg overflow-hidden overflow-y-auto h-full w-full'>
+      <Header>Header</Header>
+    </div>
+  );
+};
+
+export default Home;
