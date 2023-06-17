23e37232fdbe18f15526c12813ac09833aebc240
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-06-17 23:53:54 +0530

    Created UI for UploadModal

---
+++ components/Input.tsx
@@ -0,0 +1,20 @@
+import React, { forwardRef } from "react";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, disabled, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={`flex w-full rounded-md bg-neutal-700 border border-transparent px-3 py-3 text-sm file:border-0 file:bg-transparent file:text-sm, file:font-medium placeholder:text-neutral-400 disabled:cursor-not-allowed disabled:opacity-50 focus:outline-none ${className}`}
+        ref={ref}
+        disabled={disabled}
+        {...props}
+      />
+    );
+  }
+);
+
+Input.displayName = "Input";
+export default Input;
--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   useSessionContext,
   useSupabaseClient,
@@ -8,11 +8,23 @@ import {
 import { useRouter } from "next/navigation";
 import Modal from "./Modal";
 import useUploadModal from "@/hooks/useUploadModal";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import Input from "./Input";
+import Button from "./Button";
 
 const UploadModal = () => {
   const { session } = useSessionContext();
   const router = useRouter();
   const { onClose, isOpen } = useUploadModal();
+  const [isLoading, setIsLoading] = useState(false);
+  const { register, handleSubmit, reset } = useForm<FieldValues>({
+    defaultValues: {
+      author: "",
+      title: "",
+      song: null,
+      image: null,
+    },
+  });
 
   useEffect(() => {
     if (session) {
@@ -23,8 +35,13 @@ const UploadModal = () => {
 
   const onChange = (open: boolean) => {
     if (!open) {
+      reset();
       onClose();
     }
+  };
+
+  const onSubmit: SubmitHandler<FieldValues> = async (values) => {
+    // Upload to supabase
   };
 
   return (
@@ -34,7 +51,45 @@ const UploadModal = () => {
       isOpen={isOpen}
       onChange={onChange}
     >
-      Upload Modal
+      <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-y-4'>
+        <Input
+          id='title'
+          disabled={isLoading}
+          {...register("title", { required: true })}
+          placeholder='Song Title'
+        />
+        <Input
+          id='author'
+          disabled={isLoading}
+          {...register("author", { required: true })}
+          placeholder='Song Author Name'
+        />
+        <div>
+          <div className='pb-1'>Select a song file</div>
+          <Input
+            placeholder='test'
+            disabled={isLoading}
+            type='file'
+            accept='.mp3'
+            id='song'
+            {...register("song", { required: true })}
+          />
+        </div>
+        <div>
+          <div className='pb-1'>Select an image</div>
+          <Input
+            placeholder='test'
+            disabled={isLoading}
+            type='file'
+            accept='image/*'
+            id='image'
+            {...register("image", { required: true })}
+          />
+        </div>
+        <Button disabled={isLoading} type='submit' className='bg-green-400'>
+          Create
+        </Button>
+      </form>
     </Modal>
   );
 };
